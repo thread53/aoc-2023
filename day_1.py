5d488a873e4e65d7e13198797fcca851302b0426
thread53 <jujaka99@gmail.com>
2023-12-02 18:38:28 +0200

    Day 1 Part 2

--- day_1.py
+++ day_1.py
@@ -6,13 +6,44 @@ with open(puzzle_input, "r") as f:
     data = f.readlines()
 
 ## Part 1
-total_sum = []
+total_sum = 0
 pattern = "[1-9]"
 for line in data:
     all_digits = re.findall(pattern, line)
-   
     first = all_digits[0]
     last = all_digits[-1]
-    total_sum.append(int(first+last))
+    total_sum += (int(first+last))
 
-print(sum(total_sum))
+print(total_sum)
+
+
+## Part 2
+mapper = {
+        "one": "1",
+        "two": "2",
+        "three": "3",
+        "four": "4",
+        "five": "5",
+        "six": "6",
+        "seven": "7",
+        "eight": "8",
+        "nine": "9"
+}
+
+total_sum = 0
+pattern = "(?=(one|two|three|four|five|six|seven|eight|nine|[1-9]))"
+
+for line in data:
+    all_digits = re.findall(pattern, line)
+    first = all_digits[0]
+    last = all_digits[-1]
+    
+    if first in mapper.keys():
+        first = mapper[first]
+
+    if last in mapper.keys():
+        last = mapper[last]
+
+    total_sum += (int(first+last))
+
+print(total_sum)
